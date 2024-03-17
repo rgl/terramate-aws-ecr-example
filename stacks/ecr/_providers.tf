65efe9f779daa97dcecca8b5b45cf31185e1b475
Rui Lopes <rgl@ruilopes.com>
2024-03-17 13:01:36 +0000

    upgrade dependencies

--- stacks/ecr/_providers.tf
+++ stacks/ecr/_providers.tf
@@ -1,11 +1,11 @@
 // TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
 
 terraform {
-  required_version = "1.7.4"
+  required_version = "1.7.5"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.40.0"
+      version = "5.41.0"
     }
   }
 }
